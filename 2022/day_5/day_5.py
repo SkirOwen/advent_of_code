9d5a29413e843caab11a445a5bcec54a16e66ca4
Owen <owen.allemang@gmail.com>
2022-12-05 13:50:32 +0000

    day_5 solution

--- 2022/day_5/day_5.py
+++ 2022/day_5/day_5.py
@@ -1,23 +1,20 @@
 from collections import deque
-from typing import List
+from typing import List, Deque
 
 
-def get_init_condition(initial_position: List):
+def get_init_condition(initial_position: List) -> List[Deque]:
 	# the last line of initial_position is the index of the stacks, but has some formating
 	# split() it to get only the numbers, and taking the last one to get the total amount of stack
 	nbr_of_stack = int(initial_position[-1].split()[-1])
-	list_of_stack = [[] for i in range(nbr_of_stack)]
+	list_of_stack = [deque([]) for i in range(nbr_of_stack)]
 
-	print(list_of_stack) 
+	for v in initial_position[:-1]:
+		stack_line = list(v[1::4])	# represent the horizontal line of the stack, as if reading a line
+		for i, box in enumerate(stack_line):
+			if box != " ":
+				list_of_stack[i].appendleft(box)
 
-	for v in initial_position:
-		ind_t = [i//10 for i in range(len(v))]
-		ind_u = [i%10 for i in range(len(v))]
-		print(*ind_t)
-		print(*ind_u)
-		print(*v)
-		print(list(v[1::4]))
-		print("="*35)
+	return list_of_stack
 
 
 
@@ -33,6 +30,20 @@ def instruction_parser(instructions: List) -> List:
 	return parsed_instruction
 
 
+def move_box_9000(state: List, stack_id: int, amount: int, target: int) -> None:
+	for v in range(amount):
+		moving_box = state[stack_id].pop()
+		state[target].append(moving_box)
+
+
+def move_box_9001(state: List, stack_id: int, amount: int, target: int) -> None:
+	moving_boxes = []
+	for v in range(amount):
+		moving_boxes.append(state[stack_id].pop())
+	
+	state[target].extend(reversed(moving_boxes))
+
+
 def main() -> None:
 	filename = "input.txt"
 
@@ -42,9 +53,37 @@ def main() -> None:
 		instruction_line = lines.index("\n")
 		initial_position = lines[:instruction_line]
 		instructions = lines[instruction_line + 1:]
-
 		parsed_instruction = instruction_parser(instructions)
-		get_init_condition(initial_position)
+		
+		# Part One
+		print("==Part One==")
+		state = get_init_condition(initial_position)
+
+		for amount, stack_id, target in parsed_instruction:
+			move_box_9000(
+				state,
+				stack_id=stack_id-1,
+				amount=amount,
+				target=target-1
+				)
+
+		for v in state:
+			print(v[-1])
+
+		# Part Two
+		print("==Part Two==")
+		state = get_init_condition(initial_position)
+
+		for amount, stack_id, target in parsed_instruction:
+			move_box_9001(
+				state,
+				stack_id=stack_id-1,
+				amount=amount,
+				target=target-1
+				)
+
+		for v in state:
+			print(v[-1])
 
 
 if __name__ == '__main__':
