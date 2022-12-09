4d87281d34ffa39e1b6c5577e17c25662125f1fa
Owen <owen.allemang@gmail.com>
2022-12-09 14:58:09 +0000

    wip instruction parser

--- 2022/day_9/day_9.py
+++ 2022/day_9/day_9.py
@@ -1,10 +1,24 @@
+from typing import List
+
+
+def instruction_parser(instructions: List) -> List:
+	parsed_instruction = []
+	for line in instructions:
+		instr = line.split()
+		parsed_instruction.append((instr[0], int(instr[1])))
+	return parsed_instruction
+
+
 def main() -> None:
 	filename = "input.txt"
 
 	with open(filename, "r") as f:
 		lines = f.readlines()
 
-	print(lines)
+	instructions = instruction_parser(lines)
+	starting_position = (0, 0)
+	print(instructions) 
+
 
 if __name__ == "__main__":
 	main()
